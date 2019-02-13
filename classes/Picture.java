07ac5aad653bf4e10d7e725cb18cd91cfb7e476d
Brandon Liu <bliu20@dtechhs.org>
2019-02-13 10:15:16 -0800

    PictureGradescope

--- classes/Picture.java
+++ classes/Picture.java
@@ -6,6 +6,8 @@ import java.text.*;
 import java.util.*;
 import java.util.List; // resolves problem with java.awt.List and java.util.List
 
+package com.gradescop.pixlab;
+
 /**
  * A class that represents a picture.  This class inherits from 
  * SimplePicture and allows the student to add functionality to
